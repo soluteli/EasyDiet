8b7f1900dd20b141d4be910f9aea7012515a5b96
soluteli <soluteli@outlook.com>
2024-08-17 08:20:58 +0800

    chore: add unocss webpack plugin

--- modern.config.ts
+++ modern.config.ts
@@ -1,13 +1,25 @@
+// import PostCSSUnoCSS from '@unocss/postcss';
 import { appTools, defineConfig } from '@modern-js/app-tools';
 
 // https://modernjs.dev/en/configure/app/usage
-export default defineConfig({
-  runtime: {
-    router: true,
-  },
-  plugins: [
-    appTools({
-      bundler: 'webpack', // Set to 'experimental-rspack' to enable rspack ⚡️🦀
-    }),
-  ],
+export default defineConfig(async () => {
+  const UnoCSS = await import('@unocss/webpack');
+  return {
+    runtime: {
+      router: true,
+    },
+    plugins: [
+      appTools({
+        bundler: 'webpack', // Set to 'experimental-rspack' to enable rspack ⚡️🦀
+      }),
+    ],
+    tools: {
+      // postcss: opts => {
+      //   opts?.postcssOptions?.plugins?.push(PostCSSUnoCSS())
+      // },
+      webpackChain: async chain => {
+        chain.plugin('unocss').use(UnoCSS.default());
+      },
+    },
+  };
 });
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,4 +1,6 @@
 import { Outlet } from '@modern-js/runtime/router';
+import 'uno.css';
+import '@unocss/reset/tailwind.css';
 
 export default function Layout() {
   return (
