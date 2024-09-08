d77831cbd522c8ace310c8bc4d602e46981c1bfe
soluteli <soluteli@outlook.com>
2024-09-08 08:30:13 +0800

    feat: add dished search bar

--- src/routes/dishes/page.tsx
+++ src/routes/dishes/page.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
-import { Box, Tab, Tabs } from '@mui/material';
+import { ChangeEvent, useState } from 'react';
+import { Box, Tab, Tabs, OutlinedInput, InputAdornment } from '@mui/material';
+import { Search, HighlightOffOutlined } from '@mui/icons-material';
 import DishCard from './components/DishCard';
 import { IDish } from '@/types/dish';
 import dishesData from '@/config/dishesData.json';
@@ -7,21 +8,52 @@ import { CategoryNameMap } from '@/config/constants';
 
 export default function DishesPage() {
   const [tag, setTag] = useState('');
-  console.log('tag', tag);
+  const [keyword, setKeyword] = useState('');
 
-  const dishListData = dishesData.dishes.filter(
-    d => !tag || d.catogory === tag,
-  );
+  const dishListData = dishesData.dishes
+    .filter(d => !tag || d.catogory === tag)
+    .filter(d =>
+      d.name.toLocaleLowerCase().includes(keyword.toLocaleLowerCase()),
+    );
+
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setTag('');
+    setKeyword((e.target.value || '').trim());
+  };
+
+  const handleClear = () => {
+    setKeyword('');
+  };
 
   return (
     <Box sx={{ padding: 2 }} className="flex flex-col h-100%">
+      <Box>
+        <OutlinedInput
+          fullWidth
+          placeholder="搜索菜谱"
+          value={keyword}
+          onChange={handleSearch}
+          startAdornment={
+            <InputAdornment position="start">
+              <Search />
+            </InputAdornment>
+          }
+          endAdornment={
+            keyword.trim() ? (
+              <InputAdornment position="end">
+                <HighlightOffOutlined onClick={handleClear} />
+              </InputAdornment>
+            ) : null
+          }
+        />
+      </Box>
       <Tabs
         value={tag}
         onChange={(_e, v) => setTag(v)}
         variant="scrollable"
         scrollButtons="auto"
         allowScrollButtonsMobile
-        sx={{ minHeight: '24px' }}
+        sx={{ minHeight: '24px', my: 2 }}
       >
         {dishesData.catogories.map(c => {
           // @ts-expect-error
@@ -39,7 +71,7 @@ export default function DishesPage() {
           );
         })}
       </Tabs>
-      <Box className="flex-1 position-relative mt-2" key={tag}>
+      <Box className="flex-1 position-relative" key={tag}>
         <Box className="pos-absolute left-0 top-0 right-0 bottom-0 overflow-y-auto">
           {dishListData.map(item => {
             return (
